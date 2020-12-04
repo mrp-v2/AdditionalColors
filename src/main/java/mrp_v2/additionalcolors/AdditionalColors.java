088b8052121d10cbd7a9c740afb96cbea1e648fd
mrp-v2 <matthrewp@gmail.com>
2020-12-04 08:43:23 -0500

    inital mod creation

---
+++ src/main/java/mrp_v2/additionalcolors/AdditionalColors.java
@@ -0,0 +1,8 @@
+package mrp_v2.additionalcolors;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(AdditionalColors.ID) public class AdditionalColors
+{
+    public static final String ID = "additional" + "colors";
+}
