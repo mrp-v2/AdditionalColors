f50a942a02c051fee27c939e6179794d620acbd8
mrp-v2 <matthrewp@gmail.com>
2020-12-07 13:29:47 -0500

    remove mixin stuff

--- src/main/java/mrp_v2/additionalcolors/mixin_interfaces/ISetMaterialColor.java
+++
@@ -1,9 +0,0 @@
-package mrp_v2.additionalcolors.mixin_interfaces;
-
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.material.MaterialColor;
-
-public interface ISetMaterialColor
-{
-    AbstractBlock.Properties setMaterialColor(MaterialColor color);
-}
