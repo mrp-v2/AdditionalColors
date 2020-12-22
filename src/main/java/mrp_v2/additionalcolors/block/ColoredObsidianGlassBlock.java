497822daf085793118a80898126533def7c9572e
mrp-v2 <matthrewp@gmail.com>
2020-12-22 12:33:59 -0500

    fix crash when OE is missing

--- src/main/java/mrp_v2/additionalcolors/block/ColoredObsidianGlassBlock.java
+++ src/main/java/mrp_v2/additionalcolors/block/ColoredObsidianGlassBlock.java
@@ -1,10 +1,17 @@
 package mrp_v2.additionalcolors.block;
 
-import com.allkillernofiller.obsidianexpansion.block.ObsidianGlass;
+import mrp_v2.additionalcolors.AdditionalColors;
 import mrp_v2.additionalcolors.util.IColored;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.GlassBlock;
 import net.minecraft.item.DyeColor;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.util.Direction;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 
-public class ColoredObsidianGlassBlock extends ObsidianGlass implements IColored
+public class ColoredObsidianGlassBlock extends GlassBlock implements IColored
 {
     private final DyeColor color;
 
@@ -12,6 +19,18 @@ public class ColoredObsidianGlassBlock extends ObsidianGlass implements IColored
     {
         super(properties);
         this.color = color;
+    }
+
+    @Override public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos)
+    {
+        return true;
+    }
+
+    @Override public boolean isSideInvisible(BlockState state, BlockState adjacentBlockState, Direction side)
+    {
+        return adjacentBlockState.getBlock().asItem().isIn(ItemTags.getCollection()
+                .getTagByID(new ResourceLocation(AdditionalColors.ID, "crying_obsidian_glass"))) ||
+                super.isSideInvisible(state, adjacentBlockState, side);
     }
 
     @Override public DyeColor getColor()
